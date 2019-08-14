3b8e4a0d5cdbbf24ef99f104658ba7f6c8c5a83f
Ryan Dang <rydang@umich.edu>
2019-08-14 12:30:49 -0400

    added react time travel

--- src/index.js
+++ src/index.js
@@ -4,4 +4,7 @@ import App from "./component/App";
 import "./index.css";
 import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
+const rtt = require('react-time-travel');
+
 ReactDOM.render(<App />, document.getElementById("root"));
+rtt(document.getElementById('root'));
